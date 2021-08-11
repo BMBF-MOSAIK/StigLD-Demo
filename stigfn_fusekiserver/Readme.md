c56a2d3d9001501133ab03d926eaea6c71907f8f
Melvin Chelli <Melvin.Chelli@dfki.de>
2021-08-11 14:38:12 +0200

    Removed wrong version of stigFN

--- stigfn_fusekiserver/Readme.md
+++
@@ -1,1 +0,0 @@
-Run the project using a Java IDE like Apache Netbeans or Intellij. Requires maven 3.3.9 and JDK 15
--- stigfn_fusekiserver/agent_runner.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Work\StigFN_Fuseki
-SLEEP 2
-newman run AllRequests.postman_collection.json --iteration-count 100 --delay-request 10000
